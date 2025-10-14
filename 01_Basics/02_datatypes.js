948c3af285992443c935e1e8d5a2eed8d52a2770
Mickyvirus123 <120229172+Mickyvirus123@users.noreply.github.com>
2025-10-14 18:20:20 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 ) // its work only on browser not in nodejs
+
+  console.log(3 
+    + 
+    3)  // code readability should be high
+    console.log("Ali")
+
+
+    let name = "Ali"
+    let age = 24
+    let isLogedIn = true
+    let state;
+
+    // number => 2 to power 53
+    // bigint
+    // string => ""
+    // boolean true / false
+    // null => standalone value
+    // undefined => 
+    // symbol => unique
+
+
+
+    // object
+
+    console.log(typeof undefined); // undefined
+     console.log(typeof null) // oblect
