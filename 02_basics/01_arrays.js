6e1797f930805b61acabdd33d832ab385b6ae86e
Mickyvirus123 <120229172+Mickyvirus123@users.noreply.github.com>
2025-10-20 11:25:05 +0000

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,52 +1,52 @@
-// //Array
-// // note [ read shallow copy and deep copy] ***important***
+ //Array
+ // note [ read shallow copy and deep copy] ***important***
  const myArr = [0, 1, 2, 3, 4, 5]
-// const MyHeros = ["ironman", "thor", "spiderman"]
+ const MyHeros = ["ironman", "thor", "spiderman"]
 
-// const myArr2 = new Array(1, 2, 3, 4)
-// console.log(myArr[0]); // 0
-// console.log(myArr[1]); // 1
+ const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr[0]); // 0
+console.log(myArr[1]); // 1
 
-// // Array methods
+ // Array methods
 
-// myArr.push(6)
-// console.log(myArr); //[0, 1, 2, 3, 4, 5, 6]
+ myArr.push(6)
+ console.log(myArr); //[0, 1, 2, 3, 4, 5, 6]
 
 
  
-// myArr.push(7)
-// console.log(myArr);//[0, 2, 3, 4, 5, 6, 7]
+ myArr.push(7)
+ console.log(myArr);//[0, 2, 3, 4, 5, 6, 7]
 
 
 
-// myArr.pop()
-// console.log(myArr); //[0, 1, 2, 3, 4, 5, 6]
+ myArr.pop()
+ console.log(myArr); //[0, 1, 2, 3, 4, 5, 6]
 
 
-// myArr.unshift(8)
-// console.log(myArr); //[8, 0, 1, 2, 3, 4, 5, 6]
-
-
-
-
-// myArr.shift()
-// console.log(myArr); //[0, 1, 2, 3, 4, 5, 6]
+ myArr.unshift(8)
+ console.log(myArr); //[8, 0, 1, 2, 3, 4, 5, 6]
 
 
 
 
-
-// console.log(myArr.includes(8)); // false
-// console.log(myArr.indexOf(9)); // -1
-// console.log(myArr.indexOf(19)); // -1
-// console.log(myArr.indexOf(3)); // 3
+ myArr.shift()
+ console.log(myArr); //[0, 1, 2, 3, 4, 5, 6]
 
 
 
-// const newArr = myArr.join()
-// console.log(myArr);
-// console.log(newArr); // 0,1,2,3,4,5
-// console.log(typeof newArr); // string
+
+
+ console.log(myArr.includes(8)); // false
+ console.log(myArr.indexOf(9)); // -1
+ console.log(myArr.indexOf(19)); // -1
+ console.log(myArr.indexOf(3)); // 3
+
+
+
+ const newArr = myArr.join()
+ console.log(myArr);
+ console.log(newArr); // 0,1,2,3,4,5
+ console.log(typeof newArr); // string
 
 
 
